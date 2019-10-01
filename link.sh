5bb17a554e0e41a50ffdb42691fe457354ad1d34
jessica <jessica@lab.lan>
2019-10-01 11:31:56 +1000

    backup

--- link.sh
+++ link.sh
@@ -1,4 +1,6 @@
 ## home
 ln -s ~/dotfiles/.spacemacs ~/.spacemacs
-
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/userChrome.css ~/.mozilla/firefox/whuqgt4z.default-release/chrome/userChrome.css
+ln -s ~/dotfiles/gnome.shell.css ~/.themes/Adapta-Teal-Nokto-Eta/gnome-shell/gnome-shell.css
 # run ./link.sh to run
